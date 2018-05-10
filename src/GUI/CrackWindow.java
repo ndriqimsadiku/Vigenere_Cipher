f103b1aa04f422825c6322ad2f22eb9bb9d9f514
Ndriçim Sadiku <32226176+ndriqimsadiku@users.noreply.github.com>
2018-05-10 04:02:10 +0200

    changes on GUI CrackWindow

--- src/GUI/CrackWindow.java
+++ src/GUI/CrackWindow.java
@@ -7,7 +7,8 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
-import VigenereCipher.VigenereCipher;
+
+import VigenereCipher.VigenereCipherClass;
 
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
@@ -43,37 +44,40 @@ public class CrackWindow {
 	
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 532, 409);
+		frame.setBounds(100, 100, 532, 713);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.setLocationRelativeTo(null);
 		
 		JTextArea txtCipherCrack = new JTextArea();
-		txtCipherCrack.setBounds(31, 54, 438, 69);
+		txtCipherCrack.setFont(new Font("Monospaced", Font.PLAIN, 16));
+		txtCipherCrack.setBounds(36, 140, 438, 100);
 		frame.getContentPane().add(txtCipherCrack);
 		txtCipherCrack.setLineWrap(true);
 		txtCipherCrack.setWrapStyleWord(true);
 		
 		JTextArea txtPlainTextCrack = new JTextArea();
-		txtPlainTextCrack.setBounds(31, 188, 438, 61);
+		txtPlainTextCrack.setFont(new Font("Monospaced", Font.PLAIN, 16));
+		txtPlainTextCrack.setBounds(36, 370, 438, 100);
 		frame.getContentPane().add(txtPlainTextCrack);
 		txtPlainTextCrack.setLineWrap(true);
 		txtPlainTextCrack.setWrapStyleWord(true);
 		
-		
 		JLabel lblNewLabel = new JLabel("Ciphertext");
-		lblNewLabel.setBounds(31, 32, 79, 20);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel.setBounds(36, 112, 92, 25);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("PlainText");
-		lblNewLabel_1.setBounds(31, 167, 65, 20);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel_1.setBounds(36, 342, 83, 25);
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		JButton btnCrack = new JButton("CRACK");
 		btnCrack.setFont(new Font("Tahoma", Font.BOLD, 18));
 		btnCrack.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				VigenereCipher vigenereCipher=new VigenereCipher();
+				VigenereCipherClass vigenereCipher=new VigenereCipherClass();
 				
 				if(txtCipherCrack.getText()==null) {
 					JOptionPane.showMessageDialog(null,"Please write a text in textfield that you want to crack... ");
@@ -94,8 +98,8 @@ public class CrackWindow {
 					}
 					
 					
-				    String key = VigenereCipher.calculateKey(TextToCrack);
-				    String plainText = VigenereCipher.decrypt(TextToCrack, key);
+				    String key = VigenereCipherClass.calculateKey(TextToCrack);
+				    String plainText = VigenereCipherClass.decrypt(TextToCrack, key);
 				    txtPlainTextCrack.setText(plainText);
 				    txtCelesiCrack.setText(key);		
 					
@@ -103,17 +107,24 @@ public class CrackWindow {
 				
 			}
 		});
-		btnCrack.setBounds(120, 124, 247, 49);
+		btnCrack.setBounds(121, 260, 268, 69);
 		frame.getContentPane().add(btnCrack);
 		
 		txtCelesiCrack = new JTextField();
+		txtCelesiCrack.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		txtCelesiCrack.setColumns(10);
-		txtCelesiCrack.setBounds(41, 291, 438, 26);
+		txtCelesiCrack.setBounds(36, 546, 438, 35);
 		frame.getContentPane().add(txtCelesiCrack);
 		
-		lblelesi = new JLabel("\u00C7elesi ");
-		lblelesi.setBounds(31, 265, 79, 20);
+		lblelesi = new JLabel("\u00C7el\u00EBsi ");
+		lblelesi.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblelesi.setBounds(36, 518, 59, 25);
 		frame.getContentPane().add(lblelesi);
+		
+		JLabel lblVinegerCipher = new JLabel("Vineger Cipher");
+		lblVinegerCipher.setFont(new Font("Arial", Font.PLAIN, 40));
+		lblVinegerCipher.setBounds(124, 30, 261, 47);
+		frame.getContentPane().add(lblVinegerCipher);
 		
 	
 	}
